0ce911bdb95114aaf77005ef58766bd35496300e
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-07-15 21:19:27 -0500

    Added cleaning_tweets and storing_tweets steps

--- dags/twitter.py
+++ dags/twitter.py
@@ -1,8 +1,9 @@
 from airflow import DAG
 from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
-from data_pipelines import fetching_tweet
+from data_pipelines import fetching_tweet, cleaning_tweet
 
 """
 On UI admin/connections/create must be a file conww shii, enection called fs_tweet
@@ -22,4 +23,11 @@ with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_
                                     fs_conn_id="fs_tweet",
                                     filepath='data.csv',
                                     poke_interval=5)
+
     fetching_tweets = PythonOperator(task_id='fetching_tweets', python_callable=fetching_tweet.main)
+
+    cleaning_tweets = PythonOperator(task_id='cleaning_tweets', python_callable=cleaning_tweet.main)
+
+    storing_tweets = BashOperator(task_id='storing_tweets',
+                                  bash_command="cp /tmp/data_cleaned.csv /home/$USER/airflow/dags/data")
+
