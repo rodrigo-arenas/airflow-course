cb2502bde11c86fa70f8df5127c8a170925391a1
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-07-15 22:19:26 -0500

    Added step to upload file to postgresql

--- dags/twitter.py
+++ dags/twitter.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.postgres_operator import PostgresOperator
 from datetime import datetime
 from data_pipelines import fetching_tweet, cleaning_tweet
 
@@ -11,6 +12,8 @@ put on Extra: {"path: "/home/user/airflow/dags/data/"}
 that is the location where data.csv will be searched
 
 Run in terminal: airflow test twitter_dag waiting_for_tweets 2020-01-01
+
+psql_airflow is an airflow connection to a postgresql database with a airflow_demo db and table "tweets"
 """
 
 default_args = {
@@ -31,3 +34,8 @@ with DAG(dag_id="twitter_dag", schedule_interval="@daily", default_args=default_
     storing_tweets = BashOperator(task_id='storing_tweets',
                                   bash_command="cp /tmp/data_cleaned.csv /home/$USER/airflow/dags/data")
 
+    loading_tweets = PostgresOperator(task_id='loading_tweets',
+                                      postgres_conn_id='psql_airflow',
+                                      database='airflow_demo',
+                                      sql="COPY tweets FROM '/tmp/data_cleaned.csv' WITH ("
+                                          "FORMAT csv, HEADER TRUE, DELIMITER ',', ENCODING 'LATIN1');")
